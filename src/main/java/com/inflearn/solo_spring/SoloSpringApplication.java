5853631dc441e3848def165358d00328a4ac5da6
Jun <wnsur1234@gmail.com>
2025-04-29 20:42:49 +0900

    프로젝트 초기 commit커밋

---
+++ src/main/java/com/inflearn/solo_spring/SoloSpringApplication.java
@@ -0,0 +1,13 @@
+package com.inflearn.solo_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoloSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoloSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/inflearn/solo_spring/SoloSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inflearn.solo_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoloSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
